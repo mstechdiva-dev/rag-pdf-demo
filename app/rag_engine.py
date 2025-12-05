a5d8f61a255e9e02a35813963cd6d05ceb1f7d03
mstechdiva-dev <cmelearn@mstechdiva.com>
2025-12-04 17:53:40 -0800

    updates
    update to remove conflicts

--- app/rag_engine.py
+++ app/rag_engine.py
@@ -10,7 +10,7 @@ from typing import List, Dict, Any, Optional
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 
